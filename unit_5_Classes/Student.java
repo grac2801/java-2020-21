557639e1cb09162ec16b7b0c0eb91c1ed90fc3cc
grac2801 <oscar.gracias@icloud.com>
2020-12-07 19:43:03 -0800

    My Own Student class
    Testing class for Student class
    possible assignment

---
+++ unit_5_Classes/Student.java
@@ -0,0 +1,19 @@
+package unit_5_Classes;
+
+public class Student
+{
+	private String name;
+	private String lastName;
+	private int id;
+	private int gradeLevel;
+
+	public Student(String name, String lastName, int id, int gradeLevel)
+	{
+		super();
+		this.name = name;
+		this.lastName = lastName;
+		this.id = id;
+		this.gradeLevel = gradeLevel;
+	}
+
+}
